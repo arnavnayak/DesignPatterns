c59dadb2d27df74062c4947a9559a67a96bfc23d
arnavnayak <neilmanoj@gmail.com>
2024-05-11 18:23:53 +0530

    initial commit with builder pattern

---
+++ src/main/java/designpattern/builderpattern/builder/UserWebDTOBuilder.java
@@ -0,0 +1,51 @@
+package designpattern.builderpattern.builder;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class UserWebDTOBuilder implements UserDTOBuilder{ // it is the concrete builder which implements the UserDTOBuilder the abstract builder
+                                                            //it construct the parts and assembles final product also keep track of product created
+
+    String firstName;
+    String lastName;
+    String age;
+    String fullAddress;
+    UserWebDTO userWebDTO;
+    @Override
+    public UserDTOBuilder withFirstName(String fName) {
+        firstName = fName;
+        return this;
+    }
+
+    @Override
+    public UserDTOBuilder withLastName(String lName) {
+        lastName = lName;
+        return this;
+    }
+
+    @Override
+    public UserDTOBuilder withBirthdate(LocalDate birthdayDate) {
+        Period ageInYears =  Period.between(birthdayDate,LocalDate.now());
+        age = String.valueOf(ageInYears.getYears());
+        return this;
+    }
+
+    @Override
+    public UserDTOBuilder withAddress(Address address) {
+        fullAddress = address.getHouseNumber() + " , " +address.getStreet()+" ,\n" + address.getCity()+" , "+address.getState()+" ,\n"+address.getZipcode();
+        return this;
+    }
+
+    @Override
+    public UserWebDTO build() {
+        userWebDTO = new UserWebDTO(firstName+" "+lastName,fullAddress,age);
+        return userWebDTO;
+    }
+
+    @Override
+    public UserWebDTO getUserDTO() {
+        return userWebDTO;
+    }
+
+
+}
---
+++ src/main/java/designpattern/builderpattern/innerbuilder/UserWebDTO.java
@@ -0,0 +1,91 @@
+package designpattern.builderpattern.innerbuilder;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class UserWebDTO {// this is our product class, that is the final complex object that we want to create
+                            //this has inner builder which gives the functionality of conrete builder that is
+                            //constructing parts and assembling final product also to keep track of product created.
+
+    private String name;
+    private String address;
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+//    public UserWebDTO(String name, String address, String age) {  //commented because we are doing it by defining setters in build we can also do using const.
+//        this.name = name;
+//        this.address = address;
+//        this.age = age;
+//    }
+    @Override
+    public String toString() {
+        return  "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", age='" + age + '\'';
+    }
+
+    //inner builder
+    public static class UserWebDTOBuilder{
+        private String firstName;
+        private String lastName;
+        private String age;
+        private String fullAddress;
+        private UserWebDTO userWebDTO;
+
+        public UserWebDTOBuilder withFirstName(String fName){
+            firstName = fName;
+            return this;
+        }
+
+        public UserWebDTOBuilder withLastName(String lName){
+            lastName = lName;
+            return this;
+        }
+
+        public UserWebDTOBuilder withBirthdate(LocalDate birthDate){
+            age = Integer.toString(Period.between(birthDate,LocalDate.now()).getYears());
+            return this;
+        }
+
+        public UserWebDTOBuilder withAddress(Address address){
+            fullAddress = address.getHouseNumber() + " , " +address.getStreet()+" ,\n" + address.getCity()+" , "+address.getState()+" ,\n"+address.getZipcode();
+            return this;
+        }
+
+        public UserWebDTO build(){
+//            userWebDTO = new UserWebDTO(firstName+" "+lastName,fullAddress,age); // to do through const open this
+            this.userWebDTO = new UserWebDTO();
+            userWebDTO.setName(firstName+" "+lastName);
+            userWebDTO.setAddress(fullAddress);
+            userWebDTO.setAge(age);
+            return userWebDTO;
+        }
+
+        public UserWebDTO getUserDto(){
+            return userWebDTO;
+        }
+    }
+}
